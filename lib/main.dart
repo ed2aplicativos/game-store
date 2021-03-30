253d7bd25a1cd24b6337e38519d6b7b2dd962dee
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-03-29 21:34:56 -0400

    Bloqueando os Campos Durante o Login

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider(
+    return ChangeNotifierProvider(
       create: (_) => UserManager(),
       child: MaterialApp(
         title: 'Games Store',
