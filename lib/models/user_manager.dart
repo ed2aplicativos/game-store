ce2235298650c89aa914c1cf9a700356a2fda224
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-03-29 21:14:27 -0400

    Indicando a causa do erro no login

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,20 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
+import 'package:new_game_store/helpers/firebase_errors.dart';
 import 'package:new_game_store/models/user.dart';
 
 class UserManager {
 
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> signIn(User user) async {
+  Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      print(result.user.uid);
+      onSuccess();
     } on PlatformException catch (e) {
-      print(e);
+      onFail(getErrorString(e.code));
     }
   }
 
