464bda567f7747d4e73e28e820f0a5f464653619
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-04-02 19:40:32 -0400

    Obtendo Atualização dos Itens

--- lib/models/cart_product.dart
+++ lib/models/cart_product.dart
@@ -1,8 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:new_game_store/models/item_size.dart';
 import 'package:new_game_store/models/product.dart';
 
-class CartProduct {
+class CartProduct extends ChangeNotifier {
 
   CartProduct.fromProduct(this.product){
     productId = product.id;
@@ -52,10 +53,12 @@ class CartProduct {
 
   void increment(){
     quantity++;
+    notifyListeners();
   }
 
   void decrement(){
     quantity--;
+    notifyListeners();
   }
 
 }
