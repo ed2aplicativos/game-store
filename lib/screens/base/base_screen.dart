b8d9bd8ed074763012d2d44de15b8eb0bcabb78d
Eduardo Mendonca <edusilvamendonca@gmail.com>
2021-04-11 16:30:06 -0400

    Solicitando o Login do Usuário no Carrinho

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -7,7 +7,13 @@ import 'package:new_game_store/screens/home/home_screen.dart';
 import 'package:new_game_store/screens/products/products_screen.dart';
 import 'package:provider/provider.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
+
+  @override
+  _BaseScreenState createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
 
   @override
--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_game_store/common/empty_card.dart';
+import 'package:new_game_store/common/login_card.dart';
 import 'package:new_game_store/common/price_card.dart';
 import 'package:new_game_store/models/cart_manager.dart';
 import 'package:new_game_store/screens/cart/components/cart_tile.dart';
@@ -14,6 +16,15 @@ class CartScreen extends StatelessWidget {
       ),
       body: Consumer<CartManager>(
         builder: (_, cartManager, __){
+          if(cartManager.user == null){
+            return LoginCard();
+          }
+          if(cartManager.items.isEmpty){
+            return EmptyCard(
+              iconData: Icons.remove_shopping_cart,
+              title: 'Nenhum Produto no Carrinho',
+            );
+          }
           return ListView(
             children: <Widget>[
               Column(
